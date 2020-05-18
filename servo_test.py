ed34a66be526dd43b6d299246590350f54edbc84
nigel-otieno <52874832+nigel-otieno@users.noreply.github.com>
2020-05-17 21:21:11 -0700

    Add files via upload

--- servo_test.py
+++ servo_test.py
@@ -4,37 +4,43 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 servo1 = GPIO.PWM(11, 50)
+servo2 = GPIO.PWM(13, 50)
+
 
 servo1.start(0)
+servo2.start(0)
 print ("waiting 2 seconds")
 time.sleep(2)
 
 print ("rotating servo 90 degrees")
 duty = 2
 
-
 while True:
     while duty <= 12:
         key = input()
+        #Left Servo Control
         if key == "a":
             servo1.ChangeDutyCycle(duty)
-            time.sleep(1)
-            #servo1.stop()
-        if key == "aa":     
+            time.sleep(0.5)
+        elif key == "aa":
             servo1.ChangeDutyCycle(7)
-            time.sleep(1)
-            #servo1.stop()
-
-#while duty <= 12:
-    #servo1.ChangeDutyCycle(duty)
-    #time.sleep(1)
-    #servo1.ChangeDutyCycle(7)
-    #time.sleep(1)
-    # duty = duty + 1
+            time.sleep(0.5)
+            
+        #Right Servo Control
+        elif key == "q":
+            servo2.ChangeDutyCycle(duty)
+            time.sleep(0.5)
+        elif key == "qq":
+            servo2.ChangeDutyCycle(7)
+            time.sleep(0.5)
+            
     
 time.sleep(2)
 
 servo1.stop()
+servo2.stop()
+
 GPIO.cleanup()
 print ("good job")
