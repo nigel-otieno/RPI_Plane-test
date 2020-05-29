1abb6a31ae8d5d6fffc29771dc2b7a36ef535062
nigel-otieno <52874832+nigel-otieno@users.noreply.github.com>
2020-05-28 20:44:42 -0700

    Update servo_esc.py

--- servo_esc.py
+++ servo_esc.py
@@ -2,20 +2,18 @@ import time
 from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=8)
-#key = input()
+angle = 0
 
-#To change servo total range
-#kit.servo[0].actuation_range = 16
-#while kit.servo[0].angle <= 180:
-    #if key == "a":
-        #kit.servo[0].angle = 90
-    #if key == "aa":
-        #kit.servo[0].angle = 0
-kit.servo[0].angle = 90
-time.sleep(1)
-kit.servo[0].throttle = 180
-time.sleep(1)
-
+while angle <= 120:
+    key = input()
+    if key == "a":
+        kit.servo[0].angle = 100
+    if key == "x":
+        kit.servo[0].angle = 0
+    if key == "q":
+        kit.servo[1].angle = 100
+    if key == "w":
+        kit.servo[1].angle = 0
 
 
     
