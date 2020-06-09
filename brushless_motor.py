def110fc837c8725b5dd98192c463e93936a9cc2
nigel-otieno <52874832+nigel-otieno@users.noreply.github.com>
2020-06-09 07:44:26 -0700

    Add files via upload

--- brushless_motor.py
+++ brushless_motor.py
@@ -5,30 +5,23 @@ from adafruit_servokit import ServoKit
 pi = pigpio.pi()
 kit = ServoKit(channels=8)
 
-angle = 0
-SERVO = 4
+motor = 4
 
-while True & angle <=150:
+while motor <= 400:
     key = input()
     if key == "ready":
-        pi.set_PWM_dutycycle(SERVO, 0)
+        pi.set_PWM_dutycycle(motor, 0)
         time.sleep(3)
     if key == "go":
-        pi.set_PWM_dutycycle(SERVO, 225)
+        pi.set_PWM_dutycycle(motor, 225)
         time.sleep(3)
     if key == "set":
-        pi.set_PWM_dutycycle(SERVO, 128)
+        pi.set_PWM_dutycycle(motor, 128)
         time.sleep(3)
     if key == "a":
         kit.servo[0].angle = 100
-        time.sleep(1)
+        #time.sleep(1)
     if key == "aa":
         kit.servo[0].angle = 0
-        time.sleep(1)
-    if key == "q":
-        kit.servo[1].angle = 100
-        time.sleep(1)
-    if key == "qq":
-        kit.servo[1].angle = 0
-        time.sleep(1)
+        #time.sleep(1)
 pi.stop()
