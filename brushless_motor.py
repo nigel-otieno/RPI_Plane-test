f3d8b46356c96869474e93758ac8ffd7f8a5b785
nigel-otieno <52874832+nigel-otieno@users.noreply.github.com>
2020-06-19 00:19:20 -0700

    Add files via upload

--- brushless_motor.py
+++ brushless_motor.py
@@ -1,45 +1,51 @@
 import time
 import pigpio
-#from adafruit_servokit import ServoKit
+import pygame
+from adafruit_servokit import ServoKit
 
 pi = pigpio.pi()
-#kit = ServoKit(channels=8)
+ 
+pygame.init()
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# Initialize the joysticks
+j = pygame.joystick.Joystick(0)
+j.init()
+
+kit = ServoKit(channels=8)
 
 motor = 4
 
-while motor <= 2000:
-    key = input()
-    if key == "4":
-        pi.set_servo_pulsewidth(4, 1000)    # off
-        time.sleep(3)
-    if key == "3":
-        pi.set_servo_pulsewidth(4, 2000)    # off
-        time.sleep(3)
-    if key == "2":
-        pi.set_servo_pulsewidth(4, 1100)    # off
-        time.sleep(3)
-    if key == "1":
-        pi.set_servo_pulsewidth(4, 1500)    # off
-        time.sleep(3)
+while not done:
+    # EVENT PROCESSING STEP
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.JOYBUTTONDOWN:
+            if j.get_button(8):
+                pi.set_servo_pulsewidth(4, 1000)    # off
+                time.sleep(3)                
+                print(event.button)
+            if j.get_button(9):
+                pi.set_servo_pulsewidth(4, 1500)    # 50% power
+                time.sleep(3)                
+                print(event.button)
+            if j.get_button(0):
+                pi.set_servo_pulsewidth(4, 1800)    # 80% power
+                time.sleep(3)                
+                print(event.button)
         
-    if key == "aaa":
-        kit.servo[0].angle = 100
-        #time.sleep(1)
-    if key == "aa":
-        kit.servo[0].angle = 180
-        #time.sleep(1) 
-    if key == "a":
-        kit.servo[0].angle = 0
-        #time.sleep(1)
-        
-    if key == "q":
-        kit.servo[1].angle = 100
-        #time.sleep(1)
-    if key == "qq":
-        kit.servo[1].angle = 180
-        #time.sleep(1)
-    if key == "qqq":
-        kit.servo[1].angle = 0
-        #time.sleep(1)
+            if j.get_axis(2):
+                kit.servo[1].angle = 0
+                print(event.axis)
+
+        if event.type == pygame.JOYBUTTONUP:
+ 
+            print("Joystick button released.")
         
 pi.stop()
